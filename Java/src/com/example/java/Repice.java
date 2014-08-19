5ab48add219f5dcf7792df6e437ee4cb760418f9
Kacper Słyś <Kacperslys@gmail.com>
2014-08-19 19:02:51 +0200

    leci commit

--- Java/src/com/example/java/Repice.java
+++ Java/src/com/example/java/Repice.java
@@ -3,5 +3,10 @@ package com.example.java;
 /**
  * Created by kacper on 8/19/14.
  */
-public class Repice {
+public class Repice
+{
+    public static void main(String[] args)
+    {
+        System.out.println(" Witam Ciebie");
+    }
 }
