4f2a3bcf4e5be9cc144231e2ea08eafde0f624dc
Kacper Słyś <Kacperslys@gmail.com>
2014-08-19 20:19:05 +0200

    Cos nowego

--- Java/src/com/example/java/Repice.java
+++ Java/src/com/example/java/Repice.java
@@ -1,4 +1,5 @@
 package com.example.java;
+import java.util.Scanner;
 
 /**
  * Created by kacper on 8/19/14.
@@ -7,6 +8,31 @@ public class Repice
 {
     public static void main(String[] args)
     {
-        System.out.println(" Witam Ciebie");
+        System.out.print("Hello\n");
+        System.out.print("if you want to \n");
+        System.out.print("remove duplication press 1 \n");
+        System.out.print("remove whitespace characters press 2 \n");
+        System.out.print("remove concrete name press 3 \n");
+        Scanner scan = new Scanner(System.in);
+        int pe = scan.nextInt();
+
+        switch (pe)
+        {
+            case 1:
+                System.out.println("usuwamy powtórzenia bbleble");
+                break;
+            case 2:
+                System.out.println("usuwamy spacje");
+                break;
+            case 3:
+                System.out.println("Please enter what do you want to remove");
+                Scanner scaner = new Scanner(System.in);
+                String text =scaner.nextLine();
+                System.out.println(text);
+                break;
+
+        }
+
+
     }
 }
